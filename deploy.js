d778c196535cb64f98a0e46351b775977333936a
salmamoh905 <samiramoh246@gmail.com>
2024-12-29 08:38:33 +0300

    initial commit

---
+++ deploy.js
@@ -0,0 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const {interface, bytecode}= require('./compile.js')
+
+const provider = new HDWalletProvider(
+    
+)
